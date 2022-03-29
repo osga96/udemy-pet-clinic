c249e6e42557092d42242f809e895c2ba4f32495
osga96@gmail.com <osga96@gmail.com>
2022-03-29 14:56:48 +0200

    Initial commit.

---
+++ src/test/java/com/ineo/learn/springframework/udemypetclinic/UdemyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ineo.learn.springframework.udemypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
