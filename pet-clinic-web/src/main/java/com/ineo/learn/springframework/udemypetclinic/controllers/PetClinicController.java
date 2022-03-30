00889dd9b0f953ffadb41708fad1cbdfb059a38f
osga96@gmail.com <osga96@gmail.com>
2022-03-30 17:45:05 +0200

    Clase #64 - Crear página placeholder y controlador para index

---
+++ pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/PetClinicController.java
@@ -0,0 +1,19 @@
+package com.ineo.learn.springframework.udemypetclinic.controllers;
+
+import com.ineo.learn.springframework.udemypetclinic.services.map.OwnerServiceMap;
+import com.ineo.learn.springframework.udemypetclinic.services.map.PetServiceMap;
+import com.ineo.learn.springframework.udemypetclinic.services.map.VetServiceMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PetClinicController {
+    private OwnerServiceMap ownerServiceMap;
+    private PetServiceMap petServiceMap;
+    private VetServiceMap vetServiceMap;
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    private String index() {
+        return "index";
+    }
+}
