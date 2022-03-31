f03186cc0cf8402f467c24fe6fa9e5fb308edc36
osga96@gmail.com <osga96@gmail.com>
2022-03-31 14:41:09 +0200

    Clase #83 - Listar propietarios y veterinarios

--- pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/bootstrap/BootStrapData.java
+++ pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/bootstrap/BootStrapData.java
@@ -21,16 +21,21 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner("Persona 1", "Apellido1");
-        Owner owner2 = new Owner("Persona 2", "Apellido2");
-        Owner owner3 = new Owner("Persona 3", "Apellido3");
-
+        owner1.setId(0L);
         ownerService.save(owner1);
+        Owner owner2 = new Owner("Persona 2", "Apellido2");
+        owner2.setId(1L);
         ownerService.save(owner2);
+        Owner owner3 = new Owner("Persona 3", "Apellido3");
+        owner3.setId(2L);
         ownerService.save(owner3);
 
         Vet vet1 = new Vet("Veterinario 1", "Apellido1");
+        vet1.setId(0L);
         Vet vet2 = new Vet("Veterinario 2", "Apellido2");
+        vet2.setId(1L);
         Vet vet3 = new Vet("Veterinario 3", "Apellido3");
+        vet3.setId(2L);
 
         vetService.save(vet1);
         vetService.save(vet2);
--- pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/VetController.java
@@ -1,5 +1,6 @@
 package com.ineo.learn.springframework.udemypetclinic.controllers;
 
+import com.ineo.learn.springframework.udemypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
 
-        model.addAttribute("vets", new String[]{"test", "test", "test", "test"});
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
