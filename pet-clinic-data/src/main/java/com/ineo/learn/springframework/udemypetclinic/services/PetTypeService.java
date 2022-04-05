ec24c8c4098f0318d5d2bf438f2ac7aa4a73e3cb
osga96@gmail.com <osga96@gmail.com>
2022-04-05 10:08:25 +0200

    Clase #122 Añadir servicio y map de tipos de mascota.

---
+++ pet-clinic-data/src/main/java/com/ineo/learn/springframework/udemypetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.ineo.learn.springframework.udemypetclinic.services;
+
+import com.ineo.learn.springframework.udemypetclinic.modelPOJO.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
