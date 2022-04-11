95fa18706aad19cd7e352c74000adb8a576ed3b5
osga96@gmail.com <osga96@gmail.com>
2022-04-11 13:22:57 +0200

    Clase #148 - Añadimos mapeos JPA.

--- pet-clinic-data/src/main/java/com/ineo/learn/springframework/udemypetclinic/modelPOJO/Vet.java
+++ pet-clinic-data/src/main/java/com/ineo/learn/springframework/udemypetclinic/modelPOJO/Vet.java
@@ -1,15 +1,25 @@
 package com.ineo.learn.springframework.udemypetclinic.modelPOJO;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities;
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
         this.specialities = new HashSet<>();
+    }
+
+    public Vet() {
+
     }
 
     public Set<Speciality> getSpecialities() {
