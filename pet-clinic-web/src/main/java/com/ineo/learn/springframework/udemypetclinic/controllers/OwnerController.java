0234de7e2e8d804ebe4c6c0c26bfc3d6aaeebd72
osga96@gmail.com <osga96@gmail.com>
2022-05-03 14:21:32 +0200

    Clase #214 - Añadir ficheros html desde el proyecto de spring "spring-petclinic ", modificar plantilla detalle para mostrarla correctamente cambiando type a petType, añadir tests al controlador de propietario.

--- pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.ineo.learn.springframework.udemypetclinic.controllers;
 import com.ineo.learn.springframework.udemypetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -19,6 +22,13 @@ public class OwnerController {
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/find/index";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView modelAndView = new ModelAndView("/owners/ownerDetails");
+        modelAndView.addObject(this.ownerService.findById(ownerId));
+        return modelAndView;
     }
 
 }
---
+++ pet-clinic-web/src/test/java/com/ineo/learn/springframework/udemypetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,68 @@
+package com.ineo.learn.springframework.udemypetclinic.controllers;
+
+import com.ineo.learn.springframework.udemypetclinic.modelPOJO.Owner;
+import com.ineo.learn.springframework.udemypetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    Set<Owner> owners;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/find/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+    }
+}
