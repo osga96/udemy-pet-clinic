4b4f424a47c63626e0d34f71da8c24fa7e9d5ca9
osga96@gmail.com <osga96@gmail.com>
2022-05-09 12:27:36 +0200

    Clase #246 - Implementar objeto JSON al llamar a api de veterinarios.

--- pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ineo/learn/springframework/udemypetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.ineo.learn.springframework.udemypetclinic.controllers;
 
+import com.ineo.learn.springframework.udemypetclinic.modelPOJO.Vet;
 import com.ineo.learn.springframework.udemypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @PostMapping("/api/vets/json")
+    public @ResponseBody Set<Vet> jsonVets(Model model) {
+        return vetService.findAll();
     }
 }
